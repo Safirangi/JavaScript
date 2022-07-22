ed1988bfd6180eee68f6613144e4b57d27d5a404
Swati A Firangi <swatifirangi29@gmail.com>
2022-07-22 22:00:43 +0530

    add comments to the code

--- Form Validation/validation.js
+++ Form Validation/validation.js
@@ -2,13 +2,18 @@ function validateform() {
   var name = document.myform.name.value;
   var password = document.myform.password.value;
 
+  /*checks for empty name value*/
   if (name == null || name == "") {
     alert("Please enter name");
     return false;
-  } else if (password.length < 6) {
+  } 
+  /*checks for length of password*/
+  else if (password.length < 6) {
     alert("Password must be at least 6 characters long.");
     return false;
-  } else if (password == "password") {
+  } 
+  /*checks for password value as 'password'*/
+  else if (password == "password") {
     alert("This password is invalid");
     return false;
   }
